b42c7645f7963eee7451b6866daaf227cc518268
Adrien Chenu <adrienchenudu73@gmail.com>
2024-01-19 01:43:22 -0800

    Initial Commit

---
+++ SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Adrien CHENU on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
